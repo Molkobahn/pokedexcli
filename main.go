c5713eb36c54d700e02b2ddcd5fc4682bba87bad
Molkobahn <molkobahn@StoffesLaptop.>
2025-06-08 20:18:47 +0200

    added unit tests

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	finalSlice := make([]string, 0)
+	slice := strings.Fields(text)
+
+	for i := range slice {
+		finalSlice = append(finalSlice, slice[i])
+	}
+
+	return finalSlice
 }
